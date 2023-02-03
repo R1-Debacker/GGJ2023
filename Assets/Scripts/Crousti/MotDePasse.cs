df675595e370aa98fdcef12f8e16feed7d14f840
tite700 <tite7001@gmail.com>
2023-02-03 22:08:38 +0100

    Fonctionnalité MDP premier écran

---
+++ Assets/Scripts/Crousti/MotDePasse.cs
@@ -0,0 +1,45 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Crousti
+{
+    public class MotDePasse : MonoBehaviour
+    {
+
+        private string _motDePasse;
+        [SerializeField] private TMP_InputField inputField;
+        [SerializeField] private TextMeshProUGUI textDisplay;
+
+        public void Validation()
+        {
+            _motDePasse = inputField.text;
+            if (_motDePasse == "racines" || _motDePasse == "Racines" || _motDePasse == "RACINES" || _motDePasse == "roots" || _motDePasse == "Roots" || _motDePasse == "ROOTS")
+            {
+                textDisplay.text = "Mot de passe correct";
+                textDisplay.color = Color.green;
+            }
+            else
+            {
+                textDisplay.text = "Mot de passe incorrect";
+                textDisplay.color = Color.red;
+            }
+            inputField.text = "";
+        }
+        
+        private void Start()
+        {
+            inputField.contentType = TMP_InputField.ContentType.Password;
+            inputField.asteriskChar = '⚫';
+        }
+        
+        public void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                Validation();
+            }
+        }
+    }
+}
